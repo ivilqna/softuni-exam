1a53bb4b2a407b804c6faefc1875277ff03018fd
ivilqna <ivilqna@abv.bg>
2023-04-30 09:57:13 +0300

    Theme: add header and footer files and enqueue style

---
+++ wp-content/themes/softuni-homes/footer.php
@@ -0,0 +1,12 @@
+<footer class="site-footer">
+<p><?php echo get_bloginfo( 'title' ) ?>, Copyright <?php echo date( 'Y' ); ?>
+				<a href="#">Edits</a>,
+				<a href="#">Home</a>,
+				<a href="#">Single</a>
+			</p>
+		</footer>
+	</div>
+
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/softuni-homes/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This function takes care of handling the assets with enqueue
+ *
+ * @return void
+ */
+
+function softuni_assets() {
+    wp_enqueue_style( 'softuni-homes', get_stylesheet_directory_uri() . '/assets/css/master.css', array(), filemtime(  get_template_directory() . '/assets/css/master.css' ) );
+}
+add_action( 'wp_enqueue_scripts', 'softuni_assets' );
